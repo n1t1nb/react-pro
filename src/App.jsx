ae94f30f66f82bb608fb4c08237a560a1121f99e
n1t1nb <nitinwithbisht@gmail.com>
2022-11-19 18:09:15 +0530

    to be done

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,33 @@ import { useState } from 'react'
 
 function App() {
 
+  const [data,setData]=useState("");
+  const [darray,setdarray]=useState([]);
 
+  const handle=(e)=>{
+    setData(e.target.value);
+  }
+
+  const addData=(e)=>{
+    e.preventDefault();
+    setdarray([...darray,data]);
+    setData("");
+  }
   return (
     <>
     <h3>react</h3>
+    <form>
+      <input type="text" value={data} onChange={handle}/>
+      <button onClick={addData}>Add</button>
+
+    </form>
+    <div>
+      <ul>
+        {darray.map((value,key)=>{
+          return (<li key={key}>{value}</li>)
+        })}
+      </ul>
+    </div>
     </>
   )
 }
