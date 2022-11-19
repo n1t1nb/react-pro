699d952eb54529380110bf83d7afa6fd20898845
n1t1nb <nitinwithbisht@gmail.com>
2022-11-19 18:39:27 +0530

    search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,19 @@ function App() {
 
   const [data,setData]=useState("");
   const [darray,setdarray]=useState([]);
+  const [sdata,setSData]=useState("");
+  const [sdarray,setsdarray]=useState([]);
 
   const handle=(e)=>{
-    setData(e.target.value);
+
+    if(e.target.name=="search"){
+      setSData(e.target.value)
+      setsdarray(darray.filter((val)=> {
+        return  val.includes(sdata);
+    }))
+  }
+    else
+      setData(e.target.value);
   }
 
   const addData=(e)=>{
@@ -22,10 +32,24 @@ function App() {
       <input type="text" value={data} onChange={handle}/>
       <button onClick={addData}>Add</button>
 
+
+      <input type="text" value={sdata} onChange={handle} name="search"/>
+      
+
+
     </form>
     <div>
       <ul>
         {darray.map((value,key)=>{
+          return (<li key={key}>{value}</li>)
+        })}
+      </ul>
+    </div>
+
+    <div>
+      search result
+      <ul>
+        {sdarray.map((value,key)=>{
           return (<li key={key}>{value}</li>)
         })}
       </ul>
