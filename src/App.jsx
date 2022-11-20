ca958960e2a90d31b7f86a90415d551ecee2b09f
n1t1nb <nitinwithbisht@gmail.com>
2022-11-20 18:22:02 +0530

    added the style

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import style from './app.css';
 
 function App() {
 
@@ -17,19 +17,42 @@ function App() {
   }
   return (
     <>
-    <h3>react</h3>
+    <header className='main-header'>
+      <nav className='nav main-nav'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Store</a></li>
+        <li><a href="#">About</a></li>
+      </nav>
+    </header>
+    <h3 className='title-name'>To do List</h3>
+    
+
+    <section className='container'>
     <form>
       <input type="text" value={data} onChange={handle}/>
       <button onClick={addData}>Add</button>
 
     </form>
     <div>
-      <ul>
+
+    <div className='header'>
+      <p>Things to be done</p>
+      <ol>
         {darray.map((value,key)=>{
           return (<li key={key}>{value}</li>)
         })}
-      </ul>
+      </ol>
     </div>
+      
+    </div>
+    </section>
+    <footer>
+      <nav className='nav-footer'>
+        <li><a href='#'>facebook</a></li>
+        <li><a href='#'>twitter</a></li>
+        <li><a href='#'>instagram</a></li>
+      </nav>
+    </footer>
     </>
   )
 }
